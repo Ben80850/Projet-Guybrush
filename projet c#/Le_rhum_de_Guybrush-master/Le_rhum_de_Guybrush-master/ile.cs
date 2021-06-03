86a4d02bbc4e6a111652e47aea7c0c99f240b688
David Cabak <david.cabak@etud.u-picardie.fr>
2021-06-03 18:48:55 +0200

    Ajout de la gestion de tableau a deux dimentions

--- projet c#/Le_rhum_de_Guybrush-master/Le_rhum_de_Guybrush-master/ile.cs
+++ projet c#/Le_rhum_de_Guybrush-master/Le_rhum_de_Guybrush-master/ile.cs
@@ -39,101 +39,142 @@ namespace ile //appel par ile.chifrement
             }
         }
     }
-    class Ile
-    {
-        
-    }
     
-    class Chifrement : Ile
+    
+    class Chifrement 
     {
-       
+
         static void Main(string[] args)
         {
-            
-        int x = 0;
-          
-        string[] Carte = new string[12]; // carte 
-            int[] CarteX = new int[12]; // carte x+
-            int[] CarteXm = new int[12];// carte x-
+            int x = 0, y = 0;
+
+            string[,] Carte = new string[12, 12]; // carte 
+            int[,] CarteX = new int[12, 12]; // carte x+
+            int[,] CarteXm = new int[12, 12];// carte x-
+
+            int[,] CarteY = new int[12, 12]; // carte y+
+            int[,] CarteYm = new int[12, 12];// carte y--
 
 
             //tableau test
-            Carte[1] = "a";
-            Carte[2] = "a";
-            Carte[3] = "M";
-            Carte[4] = "M";
-            Carte[5] = "M";
-            Carte[6] = "F";
-            Carte[7] = "F";
-            Carte[8] = "F";
-            Carte[9] = "M";
-            Carte[10] = "M";
+            Carte[1, 1] = "a";
+            Carte[2, 1] = "a";
+            Carte[3, 1] = "b";
+            Carte[4, 1] = "b";
+            Carte[5, 1] = "M";
+            Carte[6, 1] = "M";
+            Carte[7, 1] = "M";
+            Carte[8, 1] = "F";
+            Carte[9, 1] = "F";
+            Carte[10, 1] = "c";
+
+            Carte[1, 2] = "a";
+            Carte[2, 2] = "a";
+            Carte[3, 2] = "b";
+            Carte[4, 2] = "b";
+            Carte[5, 2] = "M";
+            Carte[6, 2] = "M";
+            Carte[7, 2] = "M";
+            Carte[8, 2] = "F";
+            Carte[9, 2] = "F";
+            Carte[10, 2] = "c";
 
 
-
-            for (x = 1; x <= 10; x++) // pour ouest
+            for (y = 1; y <= 2; y++)
             {
-                if (Carte[x] == Carte[x + 1])
+                for (x = 1; x <= 10; x++) // pour ouest
                 {
-                    CarteX[x] = 0;
+                    if (Carte[x, y] == Carte[x + 1, y])
+                    {
+                        CarteX[x, y] = 0;
 
-                }
-                if (Carte[x] == "M" && Carte[x] == Carte[x + 1] && Carte[x] != "F")
-                {
-                    CarteX[x] = 64;
-                }
-                if (Carte[x] == "F" && Carte[x] == Carte[x + 1] && Carte[x] != "M")
-                {
-                    CarteX[x] = 32;
-                }
+                    }
+                    if (Carte[x, y] == "M" && Carte[x, y] == Carte[x + 1, y] && Carte[x, y] != "F")
+                    {
+                        CarteX[x, y] = 64;
+                    }
+                    if (Carte[x, y] == "F" && Carte[x, y] == Carte[x + 1, y] && Carte[x, y] != "M")
+                    {
+                        CarteX[x, y] = 32;
+                    }
 
-                if (Carte[x] == "M" && Carte[x] != Carte[x + 1] && Carte[x] != "F")
-                {
-                    CarteX[x] = 72;
-                }
+                    if (Carte[x, y] == "M" && Carte[x, y] != Carte[x + 1, y] && Carte[x, y] != "F")
+                    {
+                        CarteX[x, y] = 72;
+                    }
 
-                if (Carte[x] == "F" && Carte[x] != Carte[x + 1] && Carte[x] != "M")
-                {
-                    CarteX[x] = 40;
-                }
+                    if (Carte[x, y] == "F" && Carte[x, y] != Carte[x + 1, y] && Carte[x, y] != "M")
+                    {
+                        CarteX[x, y] = 40;
+                    }
 
-                if (Carte[x] != "M" && Carte[x] != Carte[x + 1] && Carte[x] != "F" || x == 10 && Carte[x] != "M" && Carte[x] != "F")
-                {
-                    CarteX[x] = 8;
+                    if (Carte[x, y] != "M" && Carte[x, y] != Carte[x + 1, y] && Carte[x, y] != "F" || x == 10 && Carte[x, y] != "M" && Carte[x, y] != "F")
+                    {
+                        CarteX[x, y] = 8;
+                    }
                 }
-
             }
 
-            for (x = 1; x <= 10; x++) // pour EST
+            for (y = 1; y <= 2; y++)
             {
-                if (Carte[x] == Carte[x - 1])
+                for (x = 1; x <= 10; x++) // pour EST
                 {
-                    CarteXm[x] = 0;
-                }
-                if (Carte[x] == "M" && Carte[x] == Carte[x - 1] || Carte[x] == "F" && Carte[x] == Carte[x - 1])
-                {
-                    CarteXm[x] = 0;
-                }
+                    if (Carte[x, y] == Carte[x - 1, y])
+                    {
+                        CarteXm[x, y] = 0;
+                    }
+                    if (Carte[x, y] == "M" && Carte[x, y] == Carte[x - 1, y] || Carte[x, y] == "F" && Carte[x, y] == Carte[x - 1, y])
+                    {
+                        CarteXm[x, y] = 0;
+                    }
 
-                if (Carte[x] == "M" && Carte[x] != Carte[x - 1] || Carte[x] == "F" && Carte[x] != Carte[x - 1])
-                {
-                    CarteXm[x] = 2;
-                }
+                    if (Carte[x, y] == "M" && Carte[x, y] != Carte[x - 1, y] || Carte[x, y] == "F" && Carte[x, y] != Carte[x - 1, y])
+                    {
+                        CarteXm[x, y] = 2;
+                    }
 
-                if (Carte[x] != "M" && Carte[x] != Carte[x - 1] || Carte[x] != "F" && Carte[x] != Carte[x - 1])
-                {
-                    CarteXm[x] = 2;
+                    if (Carte[x, y] != "M" && Carte[x, y] != Carte[x - 1, y] || Carte[x, y] != "F" && Carte[x, y] != Carte[x - 1, y])
+                    {
+                        CarteXm[x, y] = 2;
+                    }
                 }
-
             }
-             
-            for (x = 1; x <= 9; x++) //afichage 
+
+
+
+
+            for (y = 1; y <= 2; y++)
             {
-                CarteX[x] = CarteX[x] + CarteXm[x]; //fusion valeur est + ouest      
+                for (x = 1; x <= 10; x++) // haut
+                {
+                    if (Carte[x, y] != Carte[x, y + 1])
+                    {
+                        CarteY[x, y] = 0;
 
-                Console.Write("{0}:", CarteX[x]);
+                    }
+
+                    if (Carte[x, y] == Carte[x, y + 1])
+                    {
+                        CarteY[x, y] = 1;
+
+                    }
+
+
+                }
             }
-            Console.Write("{0}|", CarteX[10]);//affichage de fin de ligne
+
+
+            for (y = 1; y <= 2; y++)
+            {
+                for (x = 1; x <= 10; x++) //afichage 
+                {
+                    CarteX[x, y] = CarteX[x, y] + CarteXm[x, y]; //fusion valeur est + ouest 
+                    CarteX[x, y] = CarteX[x, y] + CarteY[x, y];
+                    Console.Write("{0};", CarteX[x, y], x);
+                    //Console.WriteLine("| ligne {0}", y);
+                }
+                Console.WriteLine();
+            }
         }
     }
 
