b177c63632a2606665bd430a60a4fb30555c55aa
David Cabak <david.cabak@etud.u-picardie.fr>
2021-06-12 12:46:05 +0200

    Compteur de lettre add

--- projet c#/Le_rhum_de_Guybrush-master/Le_rhum_de_Guybrush-master/parcelle.cs
+++ projet c#/Le_rhum_de_Guybrush-master/Le_rhum_de_Guybrush-master/parcelle.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Le_rhum_de_Guybrush_master
-{ 
+{
     class parcelle
     {
-        private List<unité> unités;
+        public char[] parcelles;
         private string Nom;
         private string type;
         private double taille;
 
-    
-    }
+        char[,] TabCarte = new char[12, 12];
 
+        public void Affichage_liste_parcelle(string carte)
+        {
+            char[] CarteChar = carte.ToCharArray();
+            char[] parcelles;
+            int x;
+            int n = 0;
+            int i = 0;
+            int compt = 0;
+
+            parcelles = new char[50];
+
+            for (x = 0; x < 99; x++)
+            {
+                while (CarteChar[n] == '\n' || CarteChar[n] == '\r') n++;
+
+                if (CarteChar[n] == 'a' + i)
+
+                {
+                    parcelles[i] = CarteChar[n];
+                    i++;
+                    n++;
+                }
+                else
+                    n++;
+            }
+
+            Console.Write(parcelles);
+            Console.WriteLine();
+
+            for (x = 0; x < 99; x++)
+            {
+                if (CarteChar[x] == 'a')
+                {
+                    compt++;
+
+                }
+
+            }
+            Console.Write("il y'a {0} unité a", compt);
+            Console.WriteLine();
+        }
+    }         
 }
+
+
+
